9aec9a57494acb6a859090c59b1f1ef9d404b71e
Eksadwimada <exsaa.madha99@gmail.com>
2024-06-06 02:32:41 +0700

    updatePakage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.serayularanganapp"
+    namespace = "com.tanxe.serayularanganapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.serayularanganapp"
+        applicationId = "com.tanxe.serayularanganapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -43,12 +43,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
@@ -57,12 +57,11 @@ dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("com.firebaseui:firebase-ui-database:8.0.0")
 
-
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
 
     implementation("xyz.sahildave:arclayout:1.0.0")
@@ -70,12 +69,12 @@ dependencies {
     implementation("io.reactivex.rxjava2:rxjava:2.2.21")
     implementation("com.jakewharton.rxbinding2:rxbinding:2.2.0")
 
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+
     implementation("com.github.yuriy-budiyev:code-scanner:2.3.0")
 
 //    libary untuk chart
-    implementation("com.google.firebase:firebase-database-ktx:20.3.1")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
     implementation("com.github.AnyChart:AnyChart-Android:1.1.5")
 }
